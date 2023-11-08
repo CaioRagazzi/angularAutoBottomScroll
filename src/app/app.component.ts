c9d4b1694815d028022f19957e59f3e7d6d159ea
Caio Ragazzi <ca.ragazzi@gmail.com>
2023-11-08 23:03:30 +0000

    feat: Auto scroll bottom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'scroll';
+export class AppComponent implements AfterViewInit {
+  messages: string[] = [];
+  @ViewChild('scrollDiv') scrollDiv!: ElementRef;
+  @ViewChildren('messages') messagesDiv!: QueryList<any>;
+
+  ngAfterViewInit() {
+    this.messagesDiv.changes.subscribe((_) => this.onItemElementsChanged());
+    const numbers = interval(500);
+    numbers.subscribe({
+      next: () => {
+        this.messages.push('oi');
+      },
+    });
+  }
+
+  private onItemElementsChanged(): void {
+    this.scrollToBottom();
+  }
+
+  private scrollToBottom(): void {
+    let nativeElement = this.scrollDiv.nativeElement;
+    nativeElement.scrollTop = nativeElement.scrollHeight;
+  }
 }
